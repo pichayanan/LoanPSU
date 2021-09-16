c61826f7b7db2a938491f5933356feb4949e6749
Watcharapon nuisin <38254231+godsnew2542@users.noreply.github.com>
2021-09-16 13:39:44 +0700

    edit file LoanAgreement.json add LoanAgreement.razor.cs add LoanAgreementModel.cs

---
+++ Models/LoanAgreementModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Models
+{
+    public class LoanAgreementModel
+    {
+        public String SingingDate { get; set; }
+        public String LoanType { get; set; }
+        public int LoanStatus { get; set; }
+        public int NoLoan { get; set; }
+        public int LoanAmout { get; set; }
+        public String DateSubmit { get; set; }
+        public int TotalAmout { get; set; }
+        public String Note { get; set; }
+    }
+}
---
+++ Pages/LoanAgreement.razor.cs
@@ -0,0 +1,52 @@
+﻿using BlazorApp.Models;
+using Microsoft.JSInterop;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Pages
+{
+    public partial class LoanAgreement
+    {
+        List<LoanAgreementModel> listAgreement;
+        string jsonLoanAgreement = "wwwroot/json/LoanAgreement.json";
+        string Message = "แสดงเฉพาะที่ยังไม่สิ้นสุดสัญญา";
+        Boolean LoanCheckbook = false;
+        string titleStatus = "";
+        string detailNote = "";
+        string dataDefault = "-";
+
+        protected override void OnInitialized()
+        {
+            listAgreement = new List<LoanAgreementModel>();
+            string jsonString = File.ReadAllText(jsonLoanAgreement);
+            listAgreement = JsonConvert.DeserializeObject<List<LoanAgreementModel>>(jsonString);
+        }
+
+        public void UploadPayment(LoanAgreementModel agreement)
+        {
+            if (agreement.LoanStatus == 4 || agreement.LoanStatus == 5 || agreement.LoanStatus == 6)
+            {
+                NavigationManager.NavigateTo("/AgreementDetail");
+            }
+        }
+
+        public async Task CheckboxSwitch()
+        {
+            var interopResult = await JS.InvokeAsync<Boolean>("checkboxSwitch");
+
+            if (interopResult == true)
+            {
+                Message = "แสดงสัญญาทั้งหมด";
+            }
+            else
+            {
+                Message = "แสดงเฉพาะที่ยังไม่สิ้นสุดสัญญา";
+            }
+            LoanCheckbook = interopResult;
+        }
+    }
+}
