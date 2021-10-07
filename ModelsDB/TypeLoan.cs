249f9f5950cd6ac97d9fc429c1001285b3f9c352
Watcharapon nuisin <38254231+godsnew2542@users.noreply.github.com>
2021-10-07 11:13:01 +0700

    add column StartDate in table TypeLoan

--- ModelsDB/TypeLoan.cs
+++ ModelsDB/TypeLoan.cs
@@ -9,13 +9,14 @@ namespace BlazorApp.ModelsDB
     {
         public decimal LoanTypeId { get; set; }
         public string LoanTypeName { get; set; }
-        public decimal LoanParentId { get; set; }
+        public decimal? LoanParentId { get; set; }
         public string LoanParentName { get; set; }
-        public decimal LoanMaxAmount { get; set; }
+        public decimal? LoanMaxAmount { get; set; }
         public string LoanInterate { get; set; }
-        public decimal LoanPeriod { get; set; }
+        public decimal? LoanPeriod { get; set; }
         public decimal Active { get; set; }
         public string Remark { get; set; }
         public string File { get; set; }
+        public DateTime? StartDate { get; set; }
     }
 }
